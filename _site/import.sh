3861aeecec327a381cdbe525ba485d0ecc358972
dietrich ayala <autonome@gmail.com>
2017-07-19 11:56:44 +0800

    migrate metafluff content to user page repo

--- _site/import.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ruby -rubygems -e 'require "jekyll-import";
-    JekyllImport::Importers::WordpressDotCom.run({
-      "source" => "/Users/dietrich/Downloads/wordpress.2015-01-02.xml",
-      "no_fetch_images" => false,
-      "assets_folder" => "assets"
-    })'
